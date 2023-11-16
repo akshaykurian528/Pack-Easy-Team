4ea6c7cc8c3287d299721a0cc5dc0c5faa5dc37b
ajkrizz <132341360+ajkrizz@users.noreply.github.com>
2023-11-16 23:03:10 +0530

    Added mainactivity

---
+++ MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.pack_easy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import com.example.pack_easy.Adapter.Adapter;
+import com.example.pack_easy.Constants.MyConstants;
+import com.example.pack_easy.Data.AppData;
+import com.example.pack_easy.Database.RoomDB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<String> titles;
+    List<Integer> images;
+    Adapter adapter;
+    RoomDB database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        recyclerView = findViewById(R.id.recyclerview);
+
+        addAddTitles();
+        addAllImages();
+        persistAppData();
+        database = RoomDB.getInstance(this);
+        System.out.println("----+----" + database.mainDao().getAllSelected(false).get(0).getItemname());
+
+        adapter=new Adapter(this,titles,images,MainActivity.this);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    public static final int TIME_INTERVAL = 2000;
+
+    private  long mBackPressed;
+
+    @Override
+    public void onBackPressed() {
+        if(mBackPressed+TIME_INTERVAL>System.currentTimeMillis()){
+            super.onBackPressed();
+            return;
+        }else{
+            Toast.makeText(this, "Tap back button to exit", Toast.LENGTH_SHORT).show();
+        }
+        mBackPressed = System.currentTimeMillis();
+    }
+
+    private void persistAppData(){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        database = RoomDB.getInstance(this);
+        AppData appData = new AppData(database);
+        int last = prefs.getInt(AppData.LAST_VERSION, 0);
+        if(!prefs.getBoolean(MyConstants.FIRST_TIME_CAMEL_CASE,false)){
+            appData.persistAllData();
+            editor.putBoolean(MyConstants.FIRST_TIME_CAMEL_CASE,true);
+            editor.commit();
+        }else if(last < AppData.NEW_VERSION){
+            database.mainDao().deleteAllSystemItems(MyConstants.SYSTEM_SMALL);
+            appData.persistAllData();
+            editor.putInt(AppData.LAST_VERSION,AppData.NEW_VERSION);
+            editor.commit();
+        }
+    }
+
+    private void addAddTitles(){
+        titles = new ArrayList<>();
+        titles.add(MyConstants.BASIC_NEEDS_CAMEL_CASE);
+        titles.add(MyConstants.CLOTHING_CAMEL_CASE);
+        titles.add(MyConstants.PERSONAL_CARE_CAMEL_CASE);
+        titles.add(MyConstants.BABY_NEEDS_CAMEL_CASE);
+        titles.add(MyConstants.HEALTH_CAMEL_CASE);
+        titles.add(MyConstants.TECHNOLOGY_CAMEL_CASE);
+        titles.add(MyConstants.FOOD_CAMEL_CASE);
+        titles.add(MyConstants.BEACH_SUPPLIES_CAMEL_CASE);
+        titles.add(MyConstants.CAR_SUPPLIES_CAMEL_CASE);
+        titles.add(MyConstants.MY_LIST_CAMEL_CASE);
+        titles.add(MyConstants.MY_SELECTIONS_CAMEL_CASE);
+        titles.add(MyConstants.REMINDER_CAMEL_CASE);
+    }
+
+    public void addAllImages(){
+        images = new ArrayList<>();
+        images.add(R.drawable.i1);
+        images.add(R.drawable.i2);
+        images.add(R.drawable.i3);
+        images.add(R.drawable.i4);
+        images.add(R.drawable.i5);
+        images.add(R.drawable.i6);
+        images.add(R.drawable.i7);
+        images.add(R.drawable.i8);
+        images.add(R.drawable.i9);
+        images.add(R.drawable.i10);
+        images.add(R.drawable.i11);
+        images.add(R.drawable.i12);
+    }
+}
